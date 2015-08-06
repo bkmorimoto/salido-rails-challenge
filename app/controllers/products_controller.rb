450e2c8ad243c7f938a201e515b594ecf85525f1
Ben Morimoto <bkmorimoto@gmail.com>
2015-08-06 12:48:03 -0400

    add relevant actions to product controller and add show view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,24 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      redirect_to edit_product_path(@product)
+    end
+  end
+
   def product_params
     params.require(:product).permit(:name, :url, :appellation, :product_type, :varietal, :vineyard, :review_url, :description, :price_max, :price_min, :price_retail, :highest_rating)
   end
