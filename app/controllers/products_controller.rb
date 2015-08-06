56e5cffd6d5c7cd8bbeda6e035cfb295abedae1c
Ben Morimoto <bkmorimoto@gmail.com>
2015-08-06 12:28:41 -0400

    able to create a new product to add to the database

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,22 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    product = Product.new(product_params)
+    if product.save
+      redirect_to root_path
+    else
+      redirect_to new_product_path
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :url, :appellation, :product_type, :varietal, :vineyard, :review_url, :description, :price_max, :price_min, :price_retail, :highest_rating)
+  end
+
 end
