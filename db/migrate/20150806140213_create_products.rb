e1eae40931e99926e77d6a72f45dc669d07f3ddc
Ben Morimoto <bkmorimoto@gmail.com>
2015-08-06 11:12:20 -0400

    fix typos in migration

--- db/migrate/20150806140213_create_products.rb
+++ db/migrate/20150806140213_create_products.rb
@@ -4,7 +4,7 @@ class CreateProducts < ActiveRecord::Migration
       t.integer :abc_id
       t.string :name
       t.string :url
-      t.stirng :appellation
+      t.string :appellation
       t.string :label
       t.string :type
       t.string :varietal
@@ -13,9 +13,9 @@ class CreateProducts < ActiveRecord::Migration
       t.text :description
       t.integer :latitude
       t.integer :longitude
-      t.decimal :price_max, scale: 2
-      t.decimal :price_min, scale: 2
-      t.decimal :price_retail, scale: 2
+      t.decimal :price_max, precision: 6, scale: 2
+      t.decimal :price_min, precision: 6, scale: 2
+      t.decimal :price_retail, precision: 6, scale: 2
       t.string :product_attributes
       t.string :highest_rating
 
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,23 @@ ActiveRecord::Schema.define(version: 20150806140213) do
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
+    t.integer  "abc_id"
     t.string   "name"
+    t.string   "url"
+    t.string   "appellation"
+    t.string   "label"
+    t.string   "type"
+    t.string   "varietal"
+    t.string   "vineyard"
+    t.string   "review_url"
+    t.text     "description"
+    t.integer  "latitude"
+    t.integer  "longitude"
+    t.decimal  "price_max",          precision: 6, scale: 2
+    t.decimal  "price_min",          precision: 6, scale: 2
+    t.decimal  "price_retail",       precision: 6, scale: 2
+    t.string   "product_attributes"
+    t.string   "highest_rating"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
