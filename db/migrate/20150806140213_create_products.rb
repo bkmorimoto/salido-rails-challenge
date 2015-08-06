9ca73875ad2c24f9b103ec6cd204ca728aa040a8
Ben Morimoto <bkmorimoto@gmail.com>
2015-08-06 11:09:25 -0400

    add relevant columns to product table for importing data from ABC WD

--- db/migrate/20150806140213_create_products.rb
+++ db/migrate/20150806140213_create_products.rb
@@ -1,7 +1,23 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+      t.integer :abc_id
       t.string :name
+      t.string :url
+      t.stirng :appellation
+      t.string :label
+      t.string :type
+      t.string :varietal
+      t.string :vineyard
+      t.string :review_url
+      t.text :description
+      t.integer :latitude
+      t.integer :longitude
+      t.decimal :price_max, scale: 2
+      t.decimal :price_min, scale: 2
+      t.decimal :price_retail, scale: 2
+      t.string :product_attributes
+      t.string :highest_rating
 
       t.timestamps
     end
